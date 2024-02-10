b980ff689ae1bde37edb2894c7d9dc8df56ad3e9
Felix Fong <fongf2@mcmaster.ca>
2024-02-10 12:04:06 -0500

    installed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import PathFindingVisualizer from "./PathFindingVisualizer";
-import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="h-100">
       <PathFindingVisualizer />
     </div>
   )
