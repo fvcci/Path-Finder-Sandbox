a8069a9992bb2e8e2da6bcaa33fee7f43b45f04d
Felix Fong <fongf2@mcmaster.ca>
2024-03-29 16:57:15 -0400

    finished getting all keyframes into tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,77 @@ export default {
         "weight-3": "rgba(233, 30, 99, 0.75)",
       },
     },
-    keyframes: {},
+    keyframes: {
+      popInCell: {
+        "0%": { transform: "scale(0.3)" },
+        "50%": { transform: "scale(1.2)" },
+        "100%": { transform: "scale(1)" },
+      },
+      popOutCell: {
+        "0%": { transform: "scale(1)" },
+        "33%": { transform: "scale(1.2)" },
+        "66%": { transform: "scale(0.3)" },
+        "100%": { transform: "scale(0)" },
+      },
+
+      visitedCell: {
+        "25%": {
+          transform: "scale(0.3)",
+          borderRadius: "100%",
+        },
+        "50%": {
+          backgroundColor: "var(--blue)",
+        },
+        "75%": {
+          transform: "scale(1.2)",
+          backgroundColor: "var(--green)",
+        },
+        "100%": {
+          transform: "scale(1)",
+          backgroundColor: "var(--turquoise)",
+        },
+      },
+      unVisitedCell: {
+        "25%": {
+          backgroundColor: "var(--turquoise)",
+        },
+        "50%": {
+          backgroundColor: "var(--green)",
+        },
+        "75%": {
+          backgroundColor: "var(--blue)",
+        },
+        "100%": {
+          backgroundColor: "none",
+        },
+      },
+
+      shortestPathCell: {
+        "0%": {
+          backgroundColor: "var(--green)",
+        },
+        "80%": {
+          backgroundColor: "var(--yellow)",
+          transform: "scale(1.3)",
+        },
+        "100%": {
+          transform: "scale(1)",
+        },
+      },
+      shortestPathCellReverse: {
+        "0%": {
+          backgroundColor: "var(--yellow)",
+        },
+        "20%": {
+          backgroundColor: "var(--green)",
+        },
+        "40%": {
+          backgroundColor: "var(--blue)",
+        },
+        "60%": {
+          backgroundColor: "none",
+        },
+      },
+    },
   },
 };
