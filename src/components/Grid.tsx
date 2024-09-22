8436f87facfa500d48b87f56a3eb5d3f52995f7c
Felix Fong <fongf2@mcmaster.ca>
2024-09-21 21:32:41 -0400

    created useMouseController.ts file

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,11 +1,9 @@
 import { useEffect } from "react";
 import * as Node from "@/lib/Node";
 import useToolBarContext from "@/hooks/useToolBarContext";
-import useBrush from "@/hooks/useBrush";
-import useMouseDraggedNode from "@/hooks/useMouseDraggedNode";
 import useGridAnimator from "@/hooks/useGridAnimator";
-import useAnimationGrid, { AnimationGrid } from "@/hooks/useAnimationGrid";
-import { assert } from "@/lib/asserts";
+import useAnimationGrid from "@/hooks/useAnimationGrid";
+import useMouseController from "@/hooks/useMouseController";
 
 export default function Grid() {
   const animationGrid = useAnimationGrid(
@@ -69,43 +67,3 @@ export default function Grid() {
     </div>
   );
 }
-
-const useMouseController = (animationGrid: AnimationGrid) => {
-  const brush = useBrush();
-  const mouseDraggedNode = useMouseDraggedNode();
-  const toolBar = useToolBarContext();
-
-  if (!animationGrid.gridForAnimation) {
-    return () => {};
-  }
-
-  return (pos: Node.Position) => ({
-    onMouseDown: () => {
-      assert(animationGrid.gridForAnimation);
-      if (
-        Node.isDestination(
-          animationGrid.gridForAnimation[pos.row][pos.col].state
-        )
-      ) {
-        mouseDraggedNode.pickUpNodeFrom(animationGrid, pos);
-        toolBar.runButton.notifyObservers("CLEAR_ALGORITHM");
-        return;
-      }
-
-      if (!toolBar.runButton.isDisplayingAlgorithm()) {
-        brush.placeBrushDownOnto(animationGrid, pos);
-      }
-    },
-    onMouseEnter: () => {
-      if (!toolBar.runButton.isDisplayingAlgorithm()) {
-        brush.drawOn(animationGrid, pos);
-      }
-
-      mouseDraggedNode.dragNodeOver(animationGrid, pos);
-    },
-    onMouseUp: () => {
-      brush.releaseBrush();
-      mouseDraggedNode.releaseNode(animationGrid);
-    },
-  });
-};
---
+++ src/hooks/useMouseController.ts
@@ -0,0 +1,46 @@
+import * as Node from "@/lib/Node";
+import useToolBarContext from "@/hooks/useToolBarContext";
+import useBrush from "@/hooks/useBrush";
+import useMouseDraggedNode from "@/hooks/useMouseDraggedNode";
+import { assert } from "@/lib/asserts";
+import { AnimationGrid } from "@/hooks/useAnimationGrid";
+
+export default function useMouseController(animationGrid: AnimationGrid) {
+  const brush = useBrush();
+  const mouseDraggedNode = useMouseDraggedNode();
+  const toolBar = useToolBarContext();
+
+  if (!animationGrid.gridForAnimation) {
+    return () => {};
+  }
+
+  return (pos: Node.Position) => ({
+    onMouseDown: () => {
+      assert(animationGrid.gridForAnimation);
+      if (
+        Node.isDestination(
+          animationGrid.gridForAnimation[pos.row][pos.col].state
+        )
+      ) {
+        mouseDraggedNode.pickUpNodeFrom(animationGrid, pos);
+        toolBar.runButton.notifyObservers("CLEAR_ALGORITHM");
+        return;
+      }
+
+      if (!toolBar.runButton.isDisplayingAlgorithm()) {
+        brush.placeBrushDownOnto(animationGrid, pos);
+      }
+    },
+    onMouseEnter: () => {
+      if (!toolBar.runButton.isDisplayingAlgorithm()) {
+        brush.drawOn(animationGrid, pos);
+      }
+
+      mouseDraggedNode.dragNodeOver(animationGrid, pos);
+    },
+    onMouseUp: () => {
+      brush.releaseBrush();
+      mouseDraggedNode.releaseNode(animationGrid);
+    },
+  });
+}
